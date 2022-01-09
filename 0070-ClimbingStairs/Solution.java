c8af50f00983dae582192f863d62a66df7af5368
anthony <ajdt703@gmail.com>
2022-01-09 13:09:06 -0800

    Add solution for climbing stairs

---
+++ 0070-ClimbingStairs/Solution.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int climbStairs(int n) {
+        if (n == 1) {
+            return 1;
+        }
+        if (n == 2) {
+            return 2;
+        }
+
+        int prevStep = 2;
+        int twoPrevStep = 1;
+        int totalSteps = 0;
+        for (int i = 3; i <= n; ++i) {
+            totalSteps = prevStep + twoPrevStep;
+            twoPrevStep = prevStep;
+            prevStep = total;
+        }
+
+        return total;
+    }
+}
